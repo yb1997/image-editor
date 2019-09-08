24eda2661e7624e07a0670f97d8640e7cd3d7f40
yogender <ybisht1997@gmail.com>
2019-09-09 01:33:23 +0530

    Setup for github pages

---
+++ docs/precache-manifest.53e523aab456040f9106b10e4c6bcfe7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "470877026773a4a6df54",
+    "url": "/static/js/main.47087702.chunk.js"
+  },
+  {
+    "revision": "f149d7ebd8a433d48ede",
+    "url": "/static/js/1.f149d7eb.chunk.js"
+  },
+  {
+    "revision": "470877026773a4a6df54",
+    "url": "/static/css/main.5b1add89.chunk.css"
+  },
+  {
+    "revision": "37efc624da1883165a13861657ad1d1a",
+    "url": "/index.html"
+  }
+];
