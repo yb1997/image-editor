0edec6da42a5dcc43685e6e5c111fd29e9d5851f
Yogender <Yogender@veridic.local>
2019-09-20 18:46:43 +0530

    Add other approach for blurring

--- src/components/adjust-tools/blur-slider.component.tsx
+++ src/components/adjust-tools/blur-slider.component.tsx
@@ -9,7 +9,7 @@ export const BlurSlider = () => {
     const ctx = useSelector<IAppStore, CanvasRenderingContext2D>(p => p.ctx);
     const [blur, setBlur] = React.useState<number>(0);
 
-    const blurImage = (imageData: ImageData, gap: number = 1) => {
+    const blurImage = (imageData: ImageData, blurLevel: number = 1) => {
         const width = imageData.width;
         const height = imageData.height;
         const data = imageData.data;
@@ -24,9 +24,9 @@ export const BlurSlider = () => {
             for(let x = 0; x < y + width; x += 4) {
                 const pixel: number[] = []; 
                 pixel.push(data[x + y]);
-                pixel.push(data[x + y]);
-                pixel.push(data[x + y]);
-                pixel.push(data[x + y]);
+                pixel.push(data[x + y + 1]);
+                pixel.push(data[x + y + 2]);
+                pixel.push(data[x + y + 3]);
                 row.push(pixel);
             }
 
@@ -37,43 +37,91 @@ export const BlurSlider = () => {
         // #region blur
         const visited = new Map<number[], boolean>();
 
-        for(let y = gap; y < imageMatrix.length; y++) {
-            for(let x = gap; x < width; x += gap * 2 + 1) {
-                const pixel: number[] = imageMatrix[y][x];
+        // #region old approach
+        // for(let y = blurLevel; y < imageMatrix.length; y++) {
+        //     for(let x = blurLevel; x < width; x += blurLevel * 2 + 1) {
+        //         const pixel: number[] = imageMatrix[y][x];
 
-                if(!visited.get(pixel)) {
-                    debugger;
-                    const leftPixel: number[] = x === 0 ? Array(4).fill(-1) : imageMatrix[y][x - 1];
-                    const rightPixel: number[] = x >= width - 1 ? Array(4).fill(-1) : imageMatrix[y][x + 1];
-                    const topPixel: number[] = y === 0 ? Array(4).fill(-1) : imageMatrix[y - 1][x];
-                    const bottomPixel: number[] = y >= height - 1 ? Array(4).fill(-1) : imageMatrix[y + 1][x];
+        //         if(!visited.get(pixel)) {
+        //             debugger;
+        //             const leftPixel: number[] = x === 0 ? Array(4).fill(-1) : imageMatrix[y][x - 1];
+        //             const rightPixel: number[] = x >= width - 1 ? Array(4).fill(-1) : imageMatrix[y][x + 1];
+        //             const topPixel: number[] = y === 0 ? Array(4).fill(-1) : imageMatrix[y - 1][x];
+        //             const bottomPixel: number[] = y >= height - 1 ? Array(4).fill(-1) : imageMatrix[y + 1][x];
 
-                    visited.set(pixel, true);
-                    if(leftPixel[0] !== -1) {
-                        visited.set(leftPixel, true);
-                        leftPixel.forEach((color, index) => {
-                            leftPixel[index] = pixel[index];
-                        });
-                    }
-                    if(rightPixel[0] !== -1)  {
-                        visited.set(rightPixel, true);
-                        rightPixel.forEach((color, index) => {
-                            rightPixel[index] = pixel[index];
-                        });
-                    }
-                    if(topPixel[0] !== -1)  {
-                        visited.set(topPixel, true);
-                        topPixel.forEach((color, index) => {
-                            topPixel[index] = pixel[index];
-                        });
-                    }
-                    if(bottomPixel[0] !== -1)  {
-                        visited.set(bottomPixel, true);
-                        bottomPixel.forEach((color, index) => {
-                            bottomPixel[index] = pixel[index];
-                        });
+        //             visited.set(pixel, true);
+        //             if(leftPixel[0] !== -1) {
+        //                 visited.set(leftPixel, true);
+        //                 leftPixel.forEach((color, index) => {
+        //                     leftPixel[index] = pixel[index];
+        //                 });
+        //             }
+        //             if(rightPixel[0] !== -1)  {
+        //                 visited.set(rightPixel, true);
+        //                 rightPixel.forEach((color, index) => {
+        //                     rightPixel[index] = pixel[index];
+        //                 });
+        //             }
+        //             if(topPixel[0] !== -1)  {
+        //                 visited.set(topPixel, true);
+        //                 topPixel.forEach((color, index) => {
+        //                     topPixel[index] = pixel[index];
+        //                 });
+        //             }
+        //             if(bottomPixel[0] !== -1)  {
+        //                 visited.set(bottomPixel, true);
+        //                 bottomPixel.forEach((color, index) => {
+        //                     bottomPixel[index] = pixel[index];
+        //                 });
+        //             }
+        //         }
+        //     }
+        // }
+        // #endregion
+
+        // new approach
+        for(let y = 0; y < height; y += blurLevel * 2 + 1) {
+            for(let x = 0; x < width; x += blurLevel * 2 + 1) {
+                
+                const initY = y - blurLevel;
+                const initX = x - blurLevel;
+                const kernelHeight = initY + (blurLevel * 2 + 1);
+                const kernelWidth = initX + (blurLevel * 2 + 1);
+
+                const reds: number[] = [];
+                const greens: number[] = [];
+                const blues: number[] = [];
+
+                debugger;
+
+                for(let y2 = initY; y2 < kernelHeight; y2++) {
+                    let rowSum = 0;
+                    for(let x2 = initX; x2 < kernelWidth; x2++) {
+                        if(imageMatrix[y2] && imageMatrix[y2][x2]) {
+                            reds.push(imageMatrix[y2][x2][0]);
+                            greens.push(imageMatrix[y2][x2][1]);
+                            blues.push(imageMatrix[y2][x2][2]);
+                        }
                     }
                 }
+
+                const red = reds.reduce((acc, color) => acc + color, 0) / reds.length;
+                const blue = blues.reduce((acc, color) => acc + color, 0) / blues.length;
+                const green = blues.reduce((acc, color) => acc + color, 0) / blues.length;
+
+                for(let y2 = initY; y2 < kernelHeight; y2++) {
+                    let rowSum = 0;
+                    for(let x2 = initX; x2 < kernelWidth; x2++) {
+                        const currentPixel = imageMatrix[y2][x2];
+                        if(currentPixel) {
+                            currentPixel[0] = red;
+                            currentPixel[1] = green;
+                            currentPixel[2] = blue;
+                        }
+                    }
+                }
+
+                console.log(imageMatrix);
             }
         }
         //#endregion
